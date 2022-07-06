318476b2cce8fa4975cb584c9814eadaa44116c8
YiyaoZhang9527 <408903228zyy@gmail.com>
2022-07-06 23:12:53 +0800

    初始化

---
+++ start_clash.sh
@@ -0,0 +1,2 @@
+cd "/home/zhangmanman/Downloads/Clash/Clash.for.Windows-0.19.19-x64-linux (1)/Clash for Windows-0.19.19-x64-linux/"
+./cfw
---
+++ start_neo4j.sh
@@ -0,0 +1,2 @@
+cd /home/zhangmanman/neo4j/neo4j-community-4.4.7-unix/neo4j-community-4.4.7/bin
+./neo4j console
